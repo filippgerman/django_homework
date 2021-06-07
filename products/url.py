3a296ee5d244b034f102b5732af225ef2c09a463
Filipp <filippgerman32@gmail.com>
2021-06-07 17:58:28 +0300

    create homework_4

--- products/url.py
+++ products/url.py
@@ -1,8 +1,13 @@
 from django.urls import path, include
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('', views.main, name='index'),
     path('products/', views.products, name='products'),
     path('contact/', views.contact, name='contact'),
 ]
+
+
