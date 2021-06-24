a66f1d2169ec5f8b12b2678432b2175970ba85e6
Filipp <filippgerman32@gmail.com>
2021-06-24 19:02:52 +0300

    create hm 8

--- products/url.py
+++ products/url.py
@@ -4,7 +4,8 @@ from . import views
 app_name = 'main'
 
 urlpatterns = [
-    path('', views.main, name='index'),
+    path('', views.indexView.as_view(), name='index'),
     path('products/', views.products, name='products'),
+    path('products/<int:category_id>', views.product_category, name='product_category'),
     path('contact/', views.contact, name='contact'),
 ]
